858e247900d2bc57b43d3309b52e982f998dfe8a
Bruno TAVERNIER <ophois34@gmail.com>
2016-10-04 15:06:41 +0200

    commentaires
    Ajout de commentaires:
    - dans le JS
    - dans le CSS

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
+/********************************
+*								*
+* Script JS de la page d'Index	*
+* Développé par B. TAVERNIER	*
+* Le 04 Octobre 2016			*
+* Copyright Le Pole S 			*
+*								*
+********************************/
 alert('Hello World!');
