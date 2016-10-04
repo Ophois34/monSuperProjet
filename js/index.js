5f8ded5c9f08e3595d48c808c18b1d44858e5d8b
Bruno TAVERNIER <ophois34@gmail.com>
2016-10-04 15:07:25 +0200

    commentaires JS
    Modification du cartouche

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 /********************************
 *								*
 * Script JS de la page d'Index	*
+* Sert à dire bonjour			*
+*								*
 * Développé par B. TAVERNIER	*
 * Le 04 Octobre 2016			*
 * Copyright Le Pole S 			*
